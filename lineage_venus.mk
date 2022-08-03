0d7631ea1e6b7fb854f73d4cda9d70f01192ecba
strwbrry143 <alexfinhart@gmail.com>
2022-08-03 23:03:28 +0800

    venus: Update Raven FP to August
    * extracted from [1]
    
    [1]: https://github.com/wuxianlin/get_android_data/blob/master/data/android_images.csv#L47
    Signed-off-by: strwbrry143 <alexfinhart@gmail.com>

--- lineage_venus.mk
+++ lineage_venus.mk
@@ -38,7 +38,7 @@ PRODUCT_NAME := lineage_venus
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220705.003.A1/8672226:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220705.004.A1/8847371:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.003.A1 8672226 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.004.A1 8847371 release-keys"
