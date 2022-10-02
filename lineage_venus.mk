839fa6368c36b394af943160bb0157515bf593a2
Tofu <ralph950412@gmail.com>
2022-10-02 13:01:10 +0800

    venus: Say goodbye to the previous maintainer * We'll miss you, Soumyo19

--- lineage_venus.mk
+++ lineage_venus.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/xiaomi/venus/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Rom flags
-RICE_MAINTAINER := Soumyo/Tofu
+RICE_MAINTAINER := Tofu
 RICE_DEVICE := Mi 11
 RICE_OFFICIAL := true
 RICE_CHIPSET := Snapdragon®888
